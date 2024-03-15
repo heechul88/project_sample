e3693ae7c28f987a47c3cdff74680b212af02e14
heechul88 <shinheechul88@gmail.com>
2024-03-15 15:09:07 +0900

    최초 커밋 프로젝트 만들기

---
+++ src/main/java/org/ex/openapi/ex_openapi/ExOpenApiApplication.java
@@ -0,0 +1,13 @@
+package org.ex.openapi.ex_openapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExOpenApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExOpenApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ex/openapi/ex_openapi/ExOpenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ex.openapi.ex_openapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExOpenApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
