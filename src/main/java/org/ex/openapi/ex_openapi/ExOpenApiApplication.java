f6b9207412d070a45617ee18db8ede072ade0d73
heechul88 <shinheechul88@gmail.com>
2024-11-10 22:52:13 +0900

    //    main 주석

--- src/main/java/org/ex/openapi/ex_openapi/ExOpenApiApplication.java
+++ src/main/java/org/ex/openapi/ex_openapi/ExOpenApiApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ExOpenApiApplication {
-//     주석 깃 테스트 01
-//     주석 깃 테스트 02
-//     주석 깃 테스트 03
-//킷 브런치 커밋 테스트 04
+//    main 주석
 
     public static void main(String[] args) {
         SpringApplication.run(ExOpenApiApplication.class, args);
