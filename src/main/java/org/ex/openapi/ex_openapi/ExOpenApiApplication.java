be42a05e4c8f9c22e72c3869d8524e6422a7f6e8
literacy_eduhansol <literacy.eduhansol@gmail.com>
2024-11-10 17:42:23 +0900

    킷 브런치 커밋 테스트 02 킷 브런치 커밋 테스트 03

--- src/main/java/org/ex/openapi/ex_openapi/ExOpenApiApplication.java
+++ src/main/java/org/ex/openapi/ex_openapi/ExOpenApiApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ExOpenApiApplication {
 //     주석 깃 테스트 01
+    //     주석 깃 테스트 02
+    //     주석 깃 테스트 03
 
     public static void main(String[] args) {
         SpringApplication.run(ExOpenApiApplication.class, args);
