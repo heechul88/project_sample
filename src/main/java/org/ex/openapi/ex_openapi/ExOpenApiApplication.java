65e21682f4b4e0f4d35c2b3b5ebbc00179943eec
heechul88 <shinheechul88@gmail.com>
2024-11-10 22:35:33 +0900

    2

--- src/main/java/org/ex/openapi/ex_openapi/ExOpenApiApplication.java
+++ src/main/java/org/ex/openapi/ex_openapi/ExOpenApiApplication.java
@@ -5,14 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ExOpenApiApplication {
-//
-//
-//
-//
-//
-//
-//
-//
+
     public static void main(String[] args) {
         SpringApplication.run(ExOpenApiApplication.class, args);
     }
