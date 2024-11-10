4c4b3c5a4dd59c5039c7eebb3f91a962f20bd7ec
heechul88 <shinheechul88@gmail.com>
2024-11-10 23:33:48 +0900

    deb_oraganiz_sub02

--- src/main/java/org/ex/openapi/ex_openapi/ExOpenApiApplication.java
+++ src/main/java/org/ex/openapi/ex_openapi/ExOpenApiApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ExOpenApiApplication {
-// TEST
-//    TEST
-//
+
     public static void main(String[] args) {
         SpringApplication.run(ExOpenApiApplication.class, args);
     }
