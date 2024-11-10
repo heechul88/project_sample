8280f27dabd808a065d324d9a17741dd5e137084
heechul88 <shinheechul88@gmail.com>
2024-11-10 23:52:18 +0900

    last_commit

--- src/main/java/org/ex/openapi/ex_openapi/config/SwaggerConfig.java
+++ src/main/java/org/ex/openapi/ex_openapi/config/SwaggerConfig.java
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+// last_comit
 @Configuration /*설정파일*/
 public class SwaggerConfig {
     @Bean
